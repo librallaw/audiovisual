e46f174834687bc8c0978a57a2ccaeeed9175946
Lawrence <librallaw@yahoo.com>
2020-01-19 04:12:59 +0100

    initial commit

---
+++ single-product.php
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>AccessDenied</Code><Message>Access Denied</Message><RequestId>4D471FEFB4F30B35</RequestId><HostId>DJSsgcTGDULsMnG1Qz3ifTyUcNX2Ft5OMW3j3VnpAtS1KiY65WUFyWK7v/hjwHHqdtBsy9F2FpM=</HostId></Error>
