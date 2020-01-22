89b5c56683fa7b032f696f549ae54cb89b99026d
librallaw <liballaw@yahoo.com>
2020-01-22 19:00:53 +0100

    Made corrections

--- categories_page.php
+++ categories_page.php
@@ -5,7 +5,7 @@
 <head>
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Pustok - Book Store HTML Template</title>
+	<title>Categories - LoveWorld Audo Visuals </title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- Use Minified Plugins Version For Fast Page Load -->
 	<link rel="stylesheet" type="text/css" media="screen" href="css/plugins.css" />
